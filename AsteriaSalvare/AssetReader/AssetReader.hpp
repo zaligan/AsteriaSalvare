f17b2789e07880a335d018df339627d8c0a63b05
zaligan <132348777+zaligan@users.noreply.github.com>
2024-02-21 22:34:36 +0900

    AssetReaderを作成

---
+++ AsteriaSalvare/AssetReader/AssetReader.hpp
@@ -0,0 +1,28 @@
+﻿#pragma once
+# include <Siv3D.hpp> // Siv3D v0.6.14
+
+/// @brief TSV形式のアセットリストを読み込み、アセットを登録します。
+class AssetReader
+{
+public:
+	/// @brief assetPath から AssetReader を構築します。
+	/// @param assetPath asset のリストが記述されたファイルのパスです。
+	AssetReader(const FilePath& assetPath);
+
+	/// @brief 初期化します。メイン関数の先頭で１度だけ呼び出してください。
+	/// @return 初期化に成功した場合 true を返します。
+	[[nodiscard]]
+	bool init();
+
+	/// @brief アセットを登録します。
+	/// @return アセットの登録に成功した場合 true を返します。
+	[[nodiscard]]
+	bool registerAsset();
+
+private:
+	/// @brief アセットリストのファイルのパスです。
+	FilePath m_assetPath;
+
+	/// @brief assetPath から読み込むTextReaderです。
+	TextReader m_reader;
+};
--- AsteriaSalvare/Main.cpp
+++ AsteriaSalvare/Main.cpp
@@ -1,11 +1,44 @@
 ﻿# include <Siv3D.hpp> // Siv3D v0.6.14
+# include "AssetReader/AssetReader.hpp"
 
 void Main()
 {
+	//window設定
+	Window::SetStyle(WindowStyle::Sizable);
+	Window::SetTitle(U"AsteriaSalvare");
+	Scene::Resize(1920, 1080);
+	Window::Resize(Size{ 1280, 720 });
+
+	// Releaseビルド時にフルスクリーンにする
+#if not _DEBUG
+	Window::SetFullscreen(true);
+#endif
+
+	System::Update();
+	
+	// AssetReaderにアセットリストのパスを渡します。
+	AssetReader assetReader{ U"Asset/asset-list.txt" };
+
+	// AssetReaderの初期化
+	if (not assetReader.init())
+	{
+		throw Error{ U"AssetReaderの初期化に失敗しました。" };
+	}
+
+	// アセットの登録
+	if (not assetReader.registerAsset())
+	{
+		throw Error{ U"アセットの登録に失敗しました。" };
+	}
+
+	//シーンマネージャーを作成
 	
 
+
+
+	// メインループ
 	while (System::Update())
 	{
-
+		TextureAsset(U"backPic").draw();
 	}
 }
